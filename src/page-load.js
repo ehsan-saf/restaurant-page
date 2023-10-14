4e08706f1baa938b67235ba65a4794208819d10c
M.Ehsan <124778596+ehsan-saf@users.noreply.github.com>
2023-10-14 14:20:36 +0330

    Create main section

--- src/page-load.js
+++ src/page-load.js
@@ -29,6 +29,19 @@ export default function loadPage() {
     header.appendChild(nav);
     // ---------------------------------------
 
+    // ------------ Main Section --------------
+    const text_main = document.createElement("p");
+    text_main.textContent = "If you've found us," +
+    "it means you don't have to look anywhere else"
+
+    const mainTitle = document.createElement("main-title");
+    mainTitle.classList.add("main-title");
+    mainTitle.appendChild(text_main);
+
+    const main = document.createElement("div");
+    main.classList.add("main");
+    main.appendChild(mainTitle);
 
     content.appendChild(header);
+    content.appendChild(main);
 }
